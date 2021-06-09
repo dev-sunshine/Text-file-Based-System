9877f4dcc1283fe4383756eb84d2cae1c2605482
Bashayer <twj.bashayer@gmail.com>
2021-06-09 18:04:20 +0300

    Add Teacher class

--- Program.cs
+++ Program.cs
@@ -2,6 +2,20 @@
 
 namespace Text_file_Based_System
 {
+    class Teacher
+     {
+         public Teacher(int id, string name, string teacherClass, string section) {
+             ID = id;
+             Name = name;
+             Class = teacherClass;
+             Section = section;
+         }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Section { get; set; }
+
+     }
     class Program
     {
         static void Main(string[] args)
