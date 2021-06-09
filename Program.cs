9df7ef66a8d7bc5c8f4f44f32e1088597bdd7846
Bashayer <twj.bashayer@gmail.com>
2021-06-09 18:21:35 +0300

    add services menu

--- Program.cs
+++ Program.cs
@@ -15,12 +15,56 @@ namespace Text_file_Based_System
         public string Class { get; set; }
         public string Section { get; set; }
 
-     }
+     } //end Teacher class
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
+            Console.WriteLine("\nHello !");
+            Console.WriteLine("\nWhich service do you want? (Enter Number Only)\n1. Add a new teacher. \n2. retrieve all teacher data. \n3. retrieve teacher data. \n4. Update teacher data.\n");
+            int serviceNum;
+            bool isNemuricValue= int.TryParse(Console.ReadLine(), out serviceNum); //if it is true value is the parsed number or 0
+            if (!isNemuricValue) {
+                Console.WriteLine("You have to enter numeric value only");
+            }
+            else {
+            int teacherID = 0;
+            Teacher teacher = null;
+            string name = "";
+            string teacherClass = "";
+            string teacherSection = "";
+            switch(serviceNum)
+            {
+                case 1:
+                Console.WriteLine("enter Teacher name:");
+                name = Console.ReadLine();
+                Console.WriteLine("enter Teacher ID:");
+                isNemuricValue = int.TryParse(Console.ReadLine(), out teacherID);
+                if (!isNemuricValue) {
+                    Console.WriteLine("enter numeric value only");
+                    break;
+                }
+                Console.WriteLine("enter Teacher class:");
+                teacherClass = Console.ReadLine();
+                Console.WriteLine("enter Teacher section:");
+                teacherSection = Console.ReadLine();
+                teacher = new Teacher(teacherID,name,teacherClass,teacherSection);
+                break;
+
+                case 2:
+                break;
+
+                case 3:
+                break;
+
+                case 4:
+                break;
+
+                default:
+                Console.WriteLine("Enter only 1 to 4.");
+                break;
+            }
+            }
+        }// end Main method
+    } //end Program class
 }
