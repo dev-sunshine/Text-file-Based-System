6bfe429b0861e8e09dcaa827845ad6c428c70acc
Bashayer <twj.bashayer@gmail.com>
2021-06-09 17:59:15 +0300

    First commit - initiate the project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Text_file_Based_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
